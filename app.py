129e21ab93a9cb10f94d12aea93fe0a81e399ba5
Abhishek Chavan <98216654+AbhishekChavan31@users.noreply.github.com>
2023-11-18 14:55:17 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pickle
+
+model = pickle.load(open('xgboost_3000.py','rb'))
+
+st.title("Brain Tumor Detection")
+st.markdown("Here we are using zernike moments extracted from the tumor images")
+
+st.subheader("Enter the Zernike moment")
+deg = st.text_input('', 0,100)
+
+st.subheader("Predicted Revenue")
+st.code(float(model.predict([[deg]])))
